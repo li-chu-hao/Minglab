865e96b520972b7c2583bb002af4dd5a97e8a5c5
li-chu-hao <161587356+li-chu-hao@users.noreply.github.com>
2026-02-09 10:02:45 +0000

    Deploying to gh-pages from @ li-chu-hao/Minglab@579f9e57c4ed0d8ef38460331ebb523c291b077b 🚀

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://li-chu-hao.github.io/Minglab//#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://li-chu-hao.github.io/Minglab/#contact">contact us</a>.'
               }
             ]
           }
