2ad19e76d046b625471250f655ba12448b2834aa
li-chu-hao <161587356+li-chu-hao@users.noreply.github.com>
2026-02-09 09:43:53 +0000

    Deploying to gh-pages from @ li-chu-hao/Minglab@8c9b2e5c0a93befe8f1d8943f9a28a206db248ee 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "alshedivat/al-folio",
+    "data-repo": "li-chu-hao/Minglab",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
